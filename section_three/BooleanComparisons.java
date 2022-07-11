fb01986e3e001c3c981946baa120f0a8f5325adc
uchario <uchario@yahoo.com>
2022-07-11 23:42:43 +0100

    added BooleanComparisons.class BooleanComparisons.java

---
+++ section_three/BooleanComparisons.java
@@ -0,0 +1,20 @@
+public class BooleanComparisons {
+    public static void main(String[] args) {
+        int chemistryGrade = 95;
+        int biologyGrade = 75;
+        int englishGrade = 75;
+
+        System.out.println(chemistryGrade < biologyGrade);
+        System.out.println(chemistryGrade >= englishGrade);
+        System.out.println(biologyGrade != englishGrade);
+
+        String sentence1 = "I love this course!";
+        String sentence2 = "I love this course!";
+        System.out.println(!sentence1.equals(sentence2));
+
+        double sales = 37.55;
+        double costs = 5.55;
+
+        System.out.println(sales > costs);
+    }
+}
