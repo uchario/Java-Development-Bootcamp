78da2bee0611442f34d51c827d76dd34bbc9e8e4
uchario <uchario@yahoo.com>
2022-07-11 15:27:55 +0100

    added Java-Bootcamp-Resources-main

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println("U    U     A");
+        System.out.println("U    U   A   A");
+        System.out.println("U    U  A     A");
+        System.out.println("U    U  AAAAAAA");
+        System.out.println("U    U  A     A");
+        System.out.println("U    U  A     A");
+        System.out.println(" UUUU   A     A");
+    }
+}
---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -0,0 +1,26 @@
+public class Biography {
+    public static void main(String[] args) {
+
+        //make a name variable
+        String name = "Uchechukwu Ariolu";
+        //make an age variable
+        int age = 26;
+        //make a country variable
+        String country = "Nigeria";
+        //make a sport variable
+        String sport = "Football";
+        //make an hours variable
+        int hours = 1;
+        //make a game variable
+        String game = "FIFA";
+        //make a subject variable
+        String subject = "Mathematics";
+        //make a grade variable
+        char grade = 'A';
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country);
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hour(s) a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored an " + grade + ".");
+
+    }
+}
