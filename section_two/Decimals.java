46fa93717798836578d1e4ea6a37c9c538fc2051
uchario <uchario@yahoo.com>
2022-07-11 21:07:53 +0100

    added Decimals.class Decimals.java

---
+++ section_two/Decimals.java
@@ -0,0 +1,10 @@
+public class Decimals {
+    public static void main(String[] args) {
+        double percentage = 55.67;
+        System.out.println("in 2020, " + percentage + "% of the world's population is urban.");
+
+        double dividend = 25;
+        double divisor = 2;
+        System.out.println(dividend/divisor);
+    }
+}
