dd1fad942b57c05ce873a7a59a74bd7247568930
uchario <uchario@yahoo.com>
2022-07-12 22:17:56 +0100

    added Detention.class modified Detention.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.1/Detention.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.1/Detention.java
@@ -1,19 +1,25 @@
 public class Detention {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        //what Bart did so far
-        System.out.println("I will not copy and paste code.");
-        System.out.println("I will use Camel Case when writing class names.");
-        System.out.println("I will use lower Camel Case when writing function names.");
-        System.out.println("I will use lower Camel Case when writing variables names.\n");
-
-        // Task 2 – call the function 6 times. 
-   }
+      //what Bart did so far
+      printLines();
+      printLines();
+      printLines();
+      printLines();
+      printLines();
+      printLines();
+      // Task 2 – call the function 6 times. 
+  }
 
 
 
     //Task 1 - Make a function here. See the doc comment for details. 
-
+  public static void printLines() {
+    System.out.println("I will not copy and paste code.");
+    System.out.println("I will use Camel Case when writing class names.");
+    System.out.println("I will use lower Camel Case when writing function names.");
+    System.out.println("I will use lower Camel Case when writing variables names.\n");
+  }
     /**    
       * Funtion name: printLines
       *
