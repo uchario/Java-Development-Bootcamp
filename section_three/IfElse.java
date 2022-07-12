d24d3c40de82b34d8fa2c494d2d263a4a2ec1533
uchario <uchario@yahoo.com>
2022-07-12 11:59:45 +0100

    added IfElse.class IfElse.java

---
+++ section_three/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int grade = 65;
+
+        if (grade > 50) {
+            System.out.println("Great job, you passed!");
+        } else {
+            System.out.print("Study hard next time.");
+        }
+    }
+}
