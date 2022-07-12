571293e34bb7289cadadc86565dcf5c1ee3223ce
uchario <uchario@yahoo.com>
2022-07-12 16:18:53 +0100

    added Calendar.class Calendar.class

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
@@ -17,7 +17,31 @@ public class Calendar {
             //  "Sunday": print ("I'm free!!"); 
             //  "otherwise": print ("that's not a day."); 
 
-        
+        switch (day) {
+            case "Monday":
+                System.out.println("Sorry, I have to staty at work late.");
+                break;
+            case "Tuesday":
+                System.out.println("It looks like I have meetings all day.");
+                break;
+            case "Wednesday":
+                System.out.println("I have a dentist appointment. Some other time!");
+                break;
+            case "Thursday":
+                System.out.println("Sorry, thursday is date night!");
+                break;
+            case "Friday":
+                System.out.println("I'm free!!");
+                break;
+            case "Saturday":
+                System.out.println("I'm free!!");
+                break;
+            case "Sunday":
+                System.out.println("I'm free!!");
+                break;
+            default:
+                System.out.println("That's not at day.");    
+        }
         
 
     }
