4bb2e8f534929f8fa87f4ec0f97ae8ba3ca54b34
uchario <uchario@yahoo.com>
2022-07-12 20:23:43 +0100

    modified Dealership.class Dealership.java

--- section_three/Dealership.java
+++ section_three/Dealership.java
@@ -12,7 +12,29 @@ public class Dealership {
 
         switch (option) {
             case "a":
-                System.out.println("You chose option: " + option);
+                System.out.println("What is your budget?");
+                int budget = scan.nextInt();
+                if (budget > 10000) {
+                    System.out.println("\nGreat! A Nissan Altima is available.");
+                    
+                    System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
+                    scan.nextLine();
+                    String insurance = scan.nextLine();
+                    
+                    System.out.println("\nDo you have a license? Write 'yes' or 'no'");
+                    String license = scan.nextLine();
+                    
+                    System.out.println("\nWhat is your credit score?");
+                    int creditScore = scan.nextInt();
+
+                    if (insurance.equals("yes") && license.equals("yes") && creditScore > 660) {
+                        System.out.println("\nSold! Pleasure doing business with you");   
+                    } else {
+                        System.out.println("\nWe're sorry! You're not eligible");
+                    }
+                } else {
+                    System.out.println("\nWe don't sell cars under $10,000. Sorry!");
+                }
                 break;
             case "b":
                 System.out.println("You chose option: " + option);
