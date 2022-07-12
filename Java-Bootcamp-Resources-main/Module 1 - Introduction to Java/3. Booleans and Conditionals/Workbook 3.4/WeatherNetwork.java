09ae48206266913b79715636715e3183257be77a
uchario <uchario@yahoo.com>
2022-07-12 15:41:36 +0100

    added WeatherNetwork.class WeatherNetwork.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
@@ -6,6 +6,13 @@ public class WeatherNetwork {
         String forecast = "";
 
         //IF - ELSE IF - ELSE STATEMENTS HERE!
+        if (temp >= -15 && temp <= -15) {
+            forecast = "The forecast is FREEZING! Stay home!";
+        } else if (temp >= 0 && temp <= 10) {
+            forecast = "The forecast is Chilly. Wear a coat!";
+        } else {
+            forecast = "It's warm. go outside!";
+        }
 
         //if temp between -15 and -1: "The forecast is FREEZING! Stay home!"
 
