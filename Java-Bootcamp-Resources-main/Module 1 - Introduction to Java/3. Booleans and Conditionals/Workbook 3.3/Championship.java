883a59241222f00ef5795a25524c13f244037695
uchario <uchario@yahoo.com>
2022-07-12 14:32:20 +0100

    added Championship.class modified Championship.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -5,17 +5,25 @@ public class Championship {
         int ravenclaw = 200;    //ravenclaw points
 
         // int margin = amount of points by which gryffindor scored over ravenclaw;
-
+        int margin = gryffindor - ravenclaw;
 
         //if gryffindor wins by a margin of 300 points:
              //print: Gryffindor takes the house cup!
+        if (margin == 300) {
+            System.out.println("Gryffindor takes the house cup!");
+        }
 
         //if gryffindor wins by a margin of any points: 
             //print: In second place, Gryffindor!
+        if (margin < 300) {
+            System.out.println("In second place, Gryffindor!");
+        }
 
         //if gryffindor loses by a margin of 100 points, they are third:
             //print: In third place, Gryffindor!
-
+        if (margin == 100) {
+            System.out.println("In third place, Gryffindor!");
+        }
         //else:
            //print: In fourth place, Gryffindor! 
         
