361bc2cade01c1ca2a8f154dafecad0c2457f5e8
uchario <uchario@yahoo.com>
2022-07-14 08:11:11 +0100

    modified ReturnValues.class ReturnValues.java

--- section_four/ReturnValues.java
+++ section_four/ReturnValues.java
@@ -1,12 +1,17 @@
 public class ReturnValues {
     public static void main(String[] args) {
-        double measure1 = measureRectangle(10.1, 0.7, "area");
+        double measure1 = measureRectangle(-10.1, 0.7, "area");
         double measure2 = measureRectangle(2.26, 89.21, "perimeter");
-        stringPrinter(measure1);
-        stringPrinter(measure2);
+        stringPrinter(measure1, "area");
+        stringPrinter(measure2, "perimeter");
     }
 
     public static double measureRectangle(double length, double width, String option) {
+        if (length < 0 || width < 0) {
+            System.out.println("Length or width cannot be negative.");
+            System.exit(0);
+        }
+
         switch (option) {
             case "area": return length * width;
             case "perimeter": return 2 * (length + width);
@@ -14,7 +19,7 @@ public class ReturnValues {
         }
     }
 
-    public static void stringPrinter(double area) {
-        System.out.println("The area of the rectangle is " + area);
+    public static void stringPrinter(double area, String option) {
+        System.out.println("The " + option + " of the rectangle is " + area);
     }
 }
