b3b77123c402cbda9b6157650df599366a5934ea
uchario <uchario@yahoo.com>
2022-07-14 08:00:12 +0100

    modified ReturnValues.class ReturnValues.java

--- section_four/ReturnValues.java
+++ section_four/ReturnValues.java
@@ -1,12 +1,17 @@
 public class ReturnValues {
     public static void main(String[] args) {
-        double measure = measureRectangle(10.1, 0.7);
-        stringPrinter(measure);
+        double measure1 = measureRectangle(10.1, 0.7, "area");
+        double measure2 = measureRectangle(2.26, 89.21, "perimeter");
+        stringPrinter(measure1);
+        stringPrinter(measure2);
     }
 
-    public static double measureRectangle(double length, double width) {
-        double area = length * width;
-        return area;
+    public static double measureRectangle(double length, double width, String option) {
+        switch (option) {
+            case "area": return length * width;
+            case "perimeter": return 2 * (length + width);
+            default: return 404;
+        }
     }
 
     public static void stringPrinter(double area) {
