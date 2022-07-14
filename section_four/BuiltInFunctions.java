e671f987f6fd10608b43d0d4987ac7482c7ccefa
uchario <uchario@yahoo.com>
2022-07-14 21:35:35 +0100

    added BuiltInFunctions.java BuiltInFunctions.class

---
+++ section_four/BuiltInFunctions.java
@@ -0,0 +1,7 @@
+public class BuiltInFunctions {
+    public static void main(String[] args) {
+        System.out.println(Math.random());
+        System.out.println(Math.sin(1.2));
+        System.out.println(Math.sqrt(5));
+    }
+}
