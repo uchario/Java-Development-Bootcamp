d3fe8256d915db2028bea779b45a4f1073419169
uchario <uchario@yahoo.com>
2022-07-16 17:02:39 +0100

    add Doubles.class modify Doubles.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
@@ -6,11 +6,21 @@ public class Doubles {
             2. Store the return values in dice1 and dice2.   
 
         */
-        int dice1;
-        int dice2;
+        int dice1 = rollDice();
+        int dice2 = rollDice();
 
-   //      System.out.println("Dice 1: " + dice1);
-   //      System.out.println("Dice 2: " + dice2 + "\n");
+        System.out.println("Dice 1: " + dice1);
+        System.out.println("Dice 2: " + dice2 + "\n");
+
+        while (dice1 != dice2) {
+            dice1 = rollDice();
+            dice2 = rollDice();
+
+            System.out.println("Dice 1: " + dice1);
+            System.out.println("Dice 2: " + dice2 + "\n");
+        }
+
+        System.out.println("You rolled doubles!");
 
         /* Task 3
             1. Set up a loop that keeps running while the two dice aren't the same.
@@ -27,7 +37,9 @@ public class Doubles {
      * @return randomNumber (int)
      * 
      */
-
+    public static int rollDice() {
+        return (int)((Math.random() * 6) + 1);
+    }
 }
 
 
