c13fbbe51d0dab5ce5ee742857e057d4679ff5fc
uchario <uchario@yahoo.com>
2022-07-16 15:50:36 +0100

    add Beer.class modify Beer.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
@@ -1,7 +1,10 @@
 public class Beer {
     public static void main(String[] args) {
        // Task 2: make a for loop that starts at i = 99, and finishes at i=1.
+       for (int i = 99; i <= 1; i--) {
+         sing(i);
     }
+   }
 
     /** Task 1
      * Function name – sing.
@@ -13,5 +16,8 @@ public class Beer {
      *      <i> bottles of beer on the wall, <i> bottles of beer!
      *      take one down, pass it around, <i-1> bottles of beer on the wall!
      */
-
+    public static void sing(int i) {
+      System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer!");
+      System.out.println("take one down, pass it around, " + (i - 1) + " bottles of beer on the wall!");
+    }
 }
