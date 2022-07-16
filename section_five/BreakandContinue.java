32698b1d91c6736cdb0eb368f6e24639f10d6e43
uchario <uchario@yahoo.com>
2022-07-16 17:17:54 +0100

    add BreakandContinue.class modify BreakandContinue.java

---
+++ section_five/BreakandContinue.java
@@ -0,0 +1,11 @@
+public class BreakandContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            if (i % 2 != 0) {
+                continue;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
