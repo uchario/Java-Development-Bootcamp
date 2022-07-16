9f8fb8d23392fce0c94d57c52855d5a2591f97fb
uchario <uchario@yahoo.com>
2022-07-16 16:25:20 +0100

    add Guess.class modify Guess.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
@@ -7,9 +7,17 @@ public class Guess  {
             1. Store a "secret" number between 1 and 5. 
             2. Then, prompt the user to enter a guess.
        */
-       System.out.print("I chose a number between 1 and 5. Try to guess it: ");
+        int secret = (int) ((Math.random() * 5) + 1);
+        
+        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
         
        Scanner scan = new Scanner(System.in);
+
+       while (scan.nextInt() != secret) {
+         System.out.println("\nGuess again: ");
+       }
+        
+       System.out.println("\nYou got it!");
 
 
         /* Task 2 
