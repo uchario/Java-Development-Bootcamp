d08ecca33aa5fb002d104c755115ce7aba0f290b
uchario <uchario@yahoo.com>
2022-07-16 16:04:18 +0100

    add CounterTool.class modify CounterTool.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
@@ -20,14 +20,23 @@ public class CounterTool {
                 pick up the user's answer 
                 
         */
-        
+        System.out.print("Pick a number to count by: ");
+        int step = scan.nextInt();
+
+        System.out.print("Pick a number to start counting from: ");
+        int start = scan.nextInt();
+
+        System.out.print("Pick a numnber to count to: ");
+        int stop = scan.nextInt();
 
       /* Task 2 – Create a for loop that:
             1. starts from the second number.
             2. stops at the third number. 
             3. counts in steps of the first.
       */
-
+        for (int i = start; i <= stop; i += step) {
+          System.out.println(i);
+        }
         scan.close();
     }
 }
