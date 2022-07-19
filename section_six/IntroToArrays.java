7169d2a4f8b73abe44bdf96add897a9357845e26
uchario <uchario@yahoo.com>
2022-07-19 16:46:18 +0100

    add IntroToArrays.class IntroToArrays.java

---
+++ section_six/IntroToArrays.java
@@ -0,0 +1,8 @@
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = {"mercia", "wessex", "northumbria", "E A"};
+        for (int i = 0; i < kingdoms.length; i++) {
+        System.out.println(kingdoms[i]);
+        }
+    }
+}
