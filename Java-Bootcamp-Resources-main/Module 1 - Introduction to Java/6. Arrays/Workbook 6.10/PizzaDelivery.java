b3a241c1cc4539f2b23caf541333a903996b4ed8
uchario <uchario@yahoo.com>
2022-07-21 23:05:26 +0100

    add PizzaDelivery.class PizzaDelivery.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.10/PizzaDelivery.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.10/PizzaDelivery.java
@@ -7,14 +7,21 @@ public class PizzaDelivery {
          *   1. Ask the user: How many pizza toppings do you want?. 
          *   2. Then, pick up the result using Scanner.
          */
-
+        System.out.println("How many pizza toppings do you want? ");
+        int result = scan.nextInt();
+        scan.nextLine();
         // Task 2 – Create the array here
-
+        String[] toppings = new String[result];
         /** Task 3
          *  print Great, enter each topping!
          *  Create a for loop that runs through the length of the array.   
          * 
          */
+        System.out.println("Great, enter each topping!");
+        for (int i = 0; i < toppings.length; i++) {
+            System.out.println(i + ". ");
+            toppings[i] = scan.nextLine();
+        }
 
         /** Task 4 – Pick up the user's toppings and store them in the array.
          *  
@@ -27,12 +34,19 @@ public class PizzaDelivery {
          *  See the workbook article for more detail   
          * 
          */
+        System.out.println("\nThank you ! Here are the toppings your ordered");
+        for (int i = 0; i < toppings.length; i++) {
+            System.out.println(toppings[i]);
+        }
 
         /** Task 6 –  Confirm the order
          *  
          *  See the workbook article for more detail  
          * 
          */
+        System.out.println("\nPress anything to confirm your order.");
+        scan.nextLine();
+        System.out.println("\nGreat a driver is on the way!");
 
         scan.close();
 
