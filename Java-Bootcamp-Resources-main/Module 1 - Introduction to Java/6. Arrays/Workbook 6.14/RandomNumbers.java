c0a8a425768c7bee21743304c1a4a3bb46e20798
uchario <uchario@yahoo.com>
2022-07-22 12:26:17 +0100

    add RandomNumbers.class modify RandomNumbers.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
@@ -1,7 +1,7 @@
 public class RandomNumbers {
     public static void main(String[] args) {
         // Task 3 – Create a 2D array with 100 rows and 10 columns.
-
+        int[][] randomArray = new int[100][10];
 
         /** Task 4
          * 
@@ -9,8 +9,12 @@ public class RandomNumbers {
          * 2. Then, pass the updated array into print2DArray.  
          * 
          */
-
-
+        for (int i = 0; i < randomArray.length; i++) {
+            for (int j = 0; j < randomArray[i].length; j++) {
+                randomArray[i][j] = randomNumber();
+            }
+        }
+        print2DArray(randomArray);
         
     }
 
@@ -22,7 +26,12 @@ public class RandomNumbers {
      * Inside the function:
      *    1. return a random number between 1 and 100. 
      */
-
+    public static int randomNumber() {
+        double randomDouble = Math.random() * 100;
+        randomDouble += 1;
+        int randomInt = (int) randomDouble;
+        return randomInt;
+    }
 
     /** Task 2
      * Function name: print2DArray
@@ -34,4 +43,12 @@ public class RandomNumbers {
      *        • Each row takes up one line.
      *        • There's a space between each value.
      */
+    public static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
 }
