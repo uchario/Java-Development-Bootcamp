89028e780984a8fbb8ce66bb1bdf55bff4e73f5f
uchario <uchario@yahoo.com>
2022-07-22 12:25:06 +0100

    add TDarrays.class add TDarrays.java

--- section_six/TDarrays.java
+++ section_six/TDarrays.java
@@ -8,6 +8,11 @@ public class TDarrays {
             {65, 58, 29, 57}
         };
         for (int i = 0; i < grades.length; i++) {
+            switch (i) {
+                case 0: System.out.print("\tHarry: "); break;
+                case 1: System.out.print("\n\tRon: "); break;
+                case 2: System.out.print("\n\tHermione: "); break;
+            }
             for (int j = 0; j < grades[i].length; j++) {
                 System.out.print(grades[i][j] + " ");
             }
