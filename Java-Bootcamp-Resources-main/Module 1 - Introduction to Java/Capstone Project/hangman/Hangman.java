1974cf84c9bb184968f7385d0a49dbd74fe3ff8a
uchario <uchario@yahoo.com>
2022-07-30 20:30:08 +0100

    modify Hangman.class Hangman.java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class Hangman {
+    public static Scanner scan = new Scanner(System.in);
 
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
     "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
@@ -67,11 +70,80 @@ public class Hangman {
     " =========\n"};
 
     public static void main(String[] args) {
+        String word = "meet";
+        System.out.println(word);
 
+        String placeholder []  = new String [word.length()];
 
+        for (int i = 0; i < word.length(); i++) {
+            placeholder[i] = "_";
+        }
 
+        String misses = "";
+        int count = 0;
 
+        for (int i = 0; i < placeholder.length; i++) {
 
+            System.out.print("\nGuess: " + " ");
+            String inputChar = scan.nextLine();
+            boolean checkGuess = checkGuess(word, inputChar);
+
+            if (checkGuess) {
+                placeholder[word.indexOf(inputChar)] = inputChar;
+            } else {
+                misses += inputChar;
+                count++;
+            }
+
+            System.out.println("\nGuess: " + inputChar);
+            System.out.println("\n" + gallows[count]);
+            System.out.print("\nWord: ");
+
+            for (int j = 0; j < placeholder.length; j++) {
+                System.out.print(placeholder[j] + " ");
+            }
+
+            System.out.println("\n\nMisses: " + misses);
+        }
+        
+    }
+
+    public static String randomWord(String[] words) {
+        double randomNumber = Math.random() * words.length;
+        randomNumber++;
+        int randInt = (int) randomNumber;
+        return words[randInt];
+    }
+
+    /*
+     * returns true if the user guessed a letter from the word correctly
+     */
+    public static boolean checkGuess(String word, String inputChar) {
+        if (word.indexOf(inputChar) == -1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /*
+     * updates the placeholders when the user makes a correct guess
+     */
+    public static void updatePlaceholders(String word, int index) {
+        
+    }
+
+    /*
+     * prints the placeholders
+     */
+    public static void printPlaceholders() {
+
+    }
+
+    /*
+     * prins guesses that the user missed
+     */
+    public static void printMissedGuesses() {
 
     }
 
