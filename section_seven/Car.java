4466b9b6e6756de65724178492784b31a780f7ff
uchario <uchario@yahoo.com>
2022-08-04 05:04:37 +0100

    modify Car.class Car.java

---
+++ section_seven/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String make;
+    double price;
+    int year;
+    String color;
+
+    public Car (String make, double price, int year, String color) {
+        this.make = make;
+        this .price = price;
+        this .year = year;
+        this.color = color;
+    }
+}
