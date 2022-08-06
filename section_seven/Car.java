33f73c6b8b5410eb1821c69682651b435609c672
uchario <uchario@yahoo.com>
2022-08-06 21:13:23 +0100

    add parts field, getter and setter

--- section_seven/Car.java
+++ section_seven/Car.java
@@ -1,14 +1,18 @@
+import java.util.Arrays;
+
 public class Car {
     private String make;
     private double price;
     private int year;
     private String color;
+    private String[] parts;
     
-    public Car (String make, double price, int year, String color) {
+    public Car (String make, double price, int year, String color, String[] parts) {
         this.make = make;
         this.price = price;
         this.year =  year;
         this.color = color;
+        this.parts = Arrays.copyOf(parts, parts.length);
     }
 
     public Car (Car source) {
@@ -16,6 +20,7 @@ public class Car {
         this.price = source.price;
         this.year = source.year;
         this.color = source.color;
+        this.parts = Arrays.copyOf(source.parts, source.parts.length);
     }
 
     public String getMake() {
@@ -34,6 +39,10 @@ public class Car {
         return this.color;
     }
 
+    public String[] getParts() {
+        return Arrays.copyOf(this.parts, this.parts.length);
+    }
+
     public void setMake(String make) {
         this.make = make;
     }
@@ -48,6 +57,10 @@ public class Car {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public void setParts(String[] parts) {
+        this.parts = Arrays.copyOf(parts, parts.length);
     }
 
     public void drive() {
