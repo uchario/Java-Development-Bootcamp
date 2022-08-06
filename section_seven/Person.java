93b7dc7356a83b8c41597479a6739d30200959b6
uchario <uchario@yahoo.com>
2022-08-06 17:48:39 +0100

    add chooseSeat() and applyPassport

--- section_seven/Person.java
+++ section_seven/Person.java
@@ -1,9 +1,9 @@
 public class Person {
-    String name;
-    String nationality;
-    String dateOfBirth;
+    private String name;
+    private String nationality;
+    private String dateOfBirth;
     String[] passport;
-    int seatNumber;
+    private int seatNumber;
 
     public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
         this.name = name;
@@ -11,12 +11,71 @@ public class Person {
         this.dateOfBirth = dateOfBirth;
         this.seatNumber = seatNumber;
     }
-    
-    public static void applyPassport() {
-        
+
+    public Person(Person source) {
+        this.name = source.name;
+        this.nationality = source.nationality;
+        this.dateOfBirth = source.dateOfBirth;
+        this.seatNumber = source.seatNumber;
     }
 
-    public static void chooseSeat() {
+    public String getName() {
+        return this.name;
+    }
 
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    public String getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public int getSeatNumber() {
+        return this.seatNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    /**
+     *  Function name: applyPassport
+     *  @return (boolean)
+     *
+     *  Inside the function:
+     *     1. Returns a random boolean of true or false.
+     */
+    public boolean applyPassport() {
+        double randomNumber = Math.random() * 2;
+        int randInt = (int) randomNumber;
+        if (randInt == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *  Function name: chooseSeat
+     *
+     *  Inside the function:
+     *    1. Sets this.seat to a random number between 1 -- 11.
+     */
+    public void chooseSeat() {
+        double randomNumber = Math.random() * 11;
+        int randInt = (int) randomNumber + 1;
+        this.setSeatNumber(randInt);
     }
 }
