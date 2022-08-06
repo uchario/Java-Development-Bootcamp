ca42e04971c188e1d50b1597f0d72d1e91637c47
uchario <uchario@yahoo.com>
2022-08-06 17:50:12 +0100

    used chooseSeat(), applyPassport() and drive()

--- section_seven/Main.java
+++ section_seven/Main.java
@@ -4,24 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Car nissan = new Car("Nissan", 2000, 2020, "Blue");
 
-        Car dodge = new Car("Dodge", 10000, 2019, "Red");
-
         Person uche = new Person("Uchechukwu Ariolu", "Nigerian", "06/11/1005", 11);
-        uche.passport = new String[]{uche.name, uche.nationality, uche.dateOfBirth};
+        //uche.passport = new String[]{uche.name, uche.nationality, uche.dateOfBirth};
 
         nissan.setColor("Jet Black");
         double newPrice = nissan.getPrice() / 2;
         nissan.setPrice(newPrice);
 
-        System.out.println(nissan.getMake());
+        nissan.drive();
+
         System.out.println(nissan.getPrice());
         System.out.println(nissan.getYear());    
         System.out.println(nissan.getColor());
 
-        System.out.println("\n" + uche.name);
-        System.out.println(uche.nationality);
-        System.out.println(uche.dateOfBirth);  
+        uche.chooseSeat();
+
+        System.out.println("\n");
+        System.out.println(uche.getName() + "\tSeat No. " + uche.getSeatNumber());
+        System.out.println(uche.getNationality());
+        System.out.println(uche.getDateOfBirth());  
         System.out.println(Arrays.toString(uche.passport));  
-        System.out.println(uche.seatNumber);
+
+        if (uche.applyPassport()) {
+            System.out.println("Congratulations " + uche.getName() + " Your passport was approved!");
+        } else {
+            System.out.println("We're sorry " + uche.getName() + " We cannot process your application");
+        }
     }
 }
