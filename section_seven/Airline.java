d6830856a6ace7c04ec0046245f07720a2519d70
uchario <uchario@yahoo.com>
2022-08-16 13:56:59 +0100

    modify createReservation()

--- section_seven/Airline.java
+++ section_seven/Airline.java
@@ -12,4 +12,22 @@ public class Airline {
     public void setPerson(Person person) {
         this.seats[person.getSeatNumber() - 1] = new Person(person);
     }
+
+    public void createReservation(Person person) {
+        while (seats[person.getSeatNumber() - 1] != null) {
+            System.out.println("\n" 
+                                + person.getName() 
+                                + ", seat:" 
+                                + person.getSeatNumber()
+                                + " seat is already taken. Please choose another one." 
+                                + "\n");
+            person.chooseSeat();
+        }
+        
+        System.out.println("Thank you "  
+                            + person.getName() 
+                            + " for flying with java airlines. Your seat number is " 
+                            + person.getSeatNumber()
+                            + ".\n");
+    }
 }
