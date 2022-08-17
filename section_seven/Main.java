1639185368790dbf2a0fcb681242b91baf87cfb8
uchario <uchario@yahoo.com>
2022-08-17 08:17:31 +0100

    implement the update to the Dealership constructor

--- section_seven/Main.java
+++ section_seven/Main.java
@@ -8,25 +8,25 @@ public class Main {
             new Car("Nissan", 6000, 2021, "yellow", new String[] {"tires", "filter"})
         };
 
-        Person[] people = new Person[] { 
-            new Person("Cleopatra", "Egypt", "69 BC", 1),
-            new Person("Alexander the Great", "Macedon", "356 BC", 2),
-            new Person("Julius Caesar", "Rome", "100 BC", 3),
-            new Person("Hannibal", "Carthage", "247 BC", 4),
-            new Person("Confucius", "China", "551 BC", 5),
-            new Person("Pericles", "Greece", "429 BC", 6),
-            new Person("Spartacus", "Thrace", "111 BC", 7),
-            new Person("Marcus Aurelius", "Rome", "121 AD", 8),
-            new Person("Leonidas", "Greece", "540 BC", 9),
-            new Person("Sun Tzu", "China", "544 BC", 10),
-            new Person("Hammurabi", "Babylon", "1750 BC", 10)
-        };
+        // Person[] people = new Person[] { 
+        //     new Person("Cleopatra", "Egypt", "69 BC", 1),
+        //     new Person("Alexander the Great", "Macedon", "356 BC", 2),
+        //     new Person("Julius Caesar", "Rome", "100 BC", 3),
+        //     new Person("Hannibal", "Carthage", "247 BC", 4),
+        //     new Person("Confucius", "China", "551 BC", 5),
+        //     new Person("Pericles", "Greece", "429 BC", 6),
+        //     new Person("Spartacus", "Thrace", "111 BC", 7),
+        //     new Person("Marcus Aurelius", "Rome", "121 AD", 8),
+        //     new Person("Leonidas", "Greece", "540 BC", 9),
+        //     new Person("Sun Tzu", "China", "544 BC", 10),
+        //     new Person("Hammurabi", "Babylon", "1750 BC", 10)
+        // };
     
-        Airline airline = new Airline();
+        // Airline airline = new Airline();
 
-        for (int i = 0; i < people.length; i++) {
-            airline.setPerson(people[i]);
-        }
+        // for (int i = 0; i < people.length; i++) {
+        //     airline.setPerson(people[i]);
+        // }
 
         // for (int i = 0; i < people.length; i++) {
         //     if (i % 2 == 0) {
@@ -36,13 +36,14 @@ public class Main {
         //     }
         // }
 
-        for (int i = 0; i < people.length; i++) {
-            airline.createReservation(people[i]);
-        }
-        System.out.println("********************** RESERVATIONS COMPLETE! **********************\n");
-        System.out.println(airline);
+        // for (int i = 0; i < people.length; i++) {
+        //     airline.createReservation(people[i]);
+        // }
+        // System.out.println("********************** RESERVATIONS COMPLETE! **********************\n");
+        // System.out.println(airline);
 
-        // Dealership dealership = new Dealership();
+        Dealership dealership = new Dealership(cars);
+        System.out.println(dealership);
 
         // for (int i = 0; i < cars.length; i++) {
         //     dealership.setCar(cars[i], i);
