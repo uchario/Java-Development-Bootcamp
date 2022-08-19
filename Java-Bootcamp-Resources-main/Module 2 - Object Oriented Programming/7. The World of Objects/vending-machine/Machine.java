c67293b3c8d1c27b102dbe146daab23294270dc3
uchario <uchario@yahoo.com>
2022-08-19 20:29:27 +0100

    change currentQuantity-- to --currentQuantity

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
@@ -1,6 +1,23 @@
 public class Machine {
     
-    
+    private Item[][] items;
+
+     public Machine (Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+     }
+
+     public Item getItem(int row, int spot) {
+        return new Item(this.items[row][spot]);
+     }
+
+     public void setItem(Item item,int row, int spot) {
+        this.items[row][spot] = new Item(item);
+     }
     
     /**
      * Function name – dispense
@@ -13,4 +30,12 @@ public class Machine {
      *      • if so: decreases its quantity by one and returns true.
      *      • otherwise: returns false.
      */
+    public boolean dispense(int row, int spot) {
+      int currentQuantity = this.items[row][spot].getQuantity();
+      if (currentQuantity > 0) {
+         this.items[row][spot].setQuantity(--currentQuantity);
+         return true;
+      }
+      return false;
+    }
 }
