0130b366b7e6a9a46fb7265dce2c3451a539b64d
uchario <uchario@yahoo.com>
2022-08-20 12:21:59 +0100

    fix bugs in Pyramid.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-four/Pyramid.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-four/Pyramid.java
@@ -1,9 +1,10 @@
 public class Pyramid {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-          for (int j = 0; j < 10; j++) {
+          for (int j = 0; j <= i; j++) {
               System.out.print(" * ");
           }
+          System.out.println("");
         }
     }
 }
