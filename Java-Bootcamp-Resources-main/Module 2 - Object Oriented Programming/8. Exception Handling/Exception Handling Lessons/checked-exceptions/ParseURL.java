bed13fcd0522e2821b25e70f172525870e713c92
uchario <uchario@yahoo.com>
2022-08-20 20:13:01 +0100

    try and catch ParseURLException

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/checked-exceptions/ParseURL.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/checked-exceptions/ParseURL.java
@@ -1,6 +1,15 @@
+import java.net.URL;
+import java.net.MalformedURLException;
 public class ParseURL {
     public static void main(String[] args) {
-
+        try {
+            URL url = new URL("https://www.google.com/images");
+            System.out.println(url.getProtocol());
+            System.out.println(url.getHost());
+            System.out.println(url.getPath());
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
