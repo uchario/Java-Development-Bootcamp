ab70e721ae5036340a85222428b0d7b546ebffaa
uchario <uchario@yahoo.com>
2022-08-20 19:36:47 +0100

    Fix tranposing a matrix bug

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-ten/Transpose.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-ten/Transpose.java
@@ -7,14 +7,21 @@ public class Transpose {
             {4, 5, 6},
             {7, 8, 9}
         };
+
+        int[][] copyMatrix = new int[][] {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+  
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = matrix[j][i];
+                copyMatrix[i][j] = matrix[j][i];
             }
         }
 
         System.out.println();
-        printMatrix(matrix);
+        printMatrix(copyMatrix);
     }
     public static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
