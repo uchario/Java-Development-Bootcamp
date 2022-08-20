cb39eee3342b2ecb9731780ba2aaede059212a5d
uchario <uchario@yahoo.com>
2022-08-20 13:49:52 +0100

    fix bugs in Matrix.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-seven/Matrix.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-seven/Matrix.java
@@ -8,8 +8,8 @@ public class Matrix {
              {0, 0, 0}
         };
 
-        for (int i = 0; i < matrix.length; i--) {
-            for (int j = 0; j < matrix.length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = i;
             }
         }
