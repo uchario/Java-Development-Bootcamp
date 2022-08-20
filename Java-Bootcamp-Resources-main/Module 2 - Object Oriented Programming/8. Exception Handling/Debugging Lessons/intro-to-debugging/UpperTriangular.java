eaee0a44397c06d0ed13e3977e9de150dd13f8a4
uchario <uchario@yahoo.com>
2022-08-20 14:00:21 +0100

    fix bugs in UpperTriangular.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Lessons/intro-to-debugging/UpperTriangular.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Lessons/intro-to-debugging/UpperTriangular.java
@@ -9,7 +9,7 @@ public class UpperTriangular {
         };
 
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = 0;
             }
         }
