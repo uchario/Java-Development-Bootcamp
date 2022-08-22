1fa9fee781317542a76110a501d4c6b4b78490c9
uchario <uchario@yahoo.com>
2022-08-22 16:38:25 +0100

    add isEmpty() check, validate Integer input, validate dealership index and empty spot

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/dealership/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/dealership/Main.java
@@ -9,20 +9,41 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         
         Car[] cars = new Car[] {
-            new Car("    ", -5000),
-            new Car("    ", 12000),
+            new Car("Nissan", 5000),
+            new Car("Porsche", 12000),
         };
-        cars[1].setMake("    ");
-        cars[1].setPrice(-8500);
+        cars[1].setMake("Dodge");
+        cars[1].setPrice(8500);
 
         Dealership dealership = new Dealership(cars);
 
         System.out.println("\n************* JAVA DEALERSHIP *************");
         while (true) {
             System.out.println(dealership);
+            if (dealership.isEmpty()) {
+                System.out.println("We're all sold out!");
+                break;
+            }
             System.out.print("Enter the spot number of the car you want to buy: "); 
+            if (!scan.hasNextInt()) {
+                scan.nextLine();
+                System.out.println("INVALID INPUT.");
+                continue;
+            }
             int spot = scan.nextInt();
+            scan.nextLine();
+            if (spot < 0 || spot > dealership.getLength()) {
+                System.out.println("INVALID INDEX");
+                continue;
+            } else if (dealership.getCar(spot) == null) {
+                System.out.println("EMPTY SPOT.");
+                continue;
+            }
             dealership.sell(spot);    
+            System.out.println("Type yes to continue shopping: ");
+            if (!scan.nextLine().equalsIgnoreCase("yes")) {
+                break;
+            }
         }
 
        //scan.close();
