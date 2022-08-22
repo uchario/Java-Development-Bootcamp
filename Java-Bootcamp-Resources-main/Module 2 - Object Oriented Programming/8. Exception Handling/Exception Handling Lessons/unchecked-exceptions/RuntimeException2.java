5e17e1f3cdc5f0b9eaea4e5b7e770dbacd86d68b
uchario <uchario@yahoo.com>
2022-08-22 11:30:57 +0100

    Cater to the null exception

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/unchecked-exceptions/RuntimeException2.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/unchecked-exceptions/RuntimeException2.java
@@ -1,6 +1,12 @@
 public class RuntimeException2 {
     public static void main(String[] args) {
         String word = null;
+        if (word == null) {
+            System.out.println("Please enter a valid word");
+        } else {
+            word.toLowerCase();
+        };
+     
         
     }
 }
