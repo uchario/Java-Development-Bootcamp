0541d06fcd6f75b9a2fbcc9ed588de62f3f2ceb2
uchario <uchario@yahoo.com>
2022-08-23 11:36:07 +0100

    populate items array, request user input for 'row' and 'spot', valid entries by user in Main.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/Main.java
@@ -13,9 +13,9 @@ public class Main {
         System.out.println("\t************************************************");
 
         Item[][] items = new Item[][] {
-            { new Item("Pepsi", 1.99, -3) , new Item("Fresca", 1.49, 3), new Item("     ", 2.49, -2) },
-            { new Item("Fanta", 1.99, 2) , new Item("      ", 1.49, 2), new Item("A & W", 2.49, 3) },
-            { new Item("     ", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", -2.49, 1) }
+            { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Ginger", 2.49, 2) },
+            { new Item("Fanta", 1.99, 2) , new Item("Crush", 1.49, 2), new Item("A & W", 2.49, 3) },
+            { new Item("Bigi", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
         };   
 
         Machine machine = new Machine(items);
@@ -24,9 +24,22 @@ public class Main {
 
         while (true) {
             System.out.print("Pick a row: ");
-            int row = scan.nextInt();
+            int row = scan.hasNextInt() ? scan.nextInt() : 404;
+            scan.nextLine();
             System.out.print("Pick a spot in the row: ");
-            int spot = scan.nextInt();
+            int spot = scan.hasNextInt() ? scan.nextInt() : 404;
+            scan.nextLine();
+
+            if (row == 404 || spot == 404) {
+                System.out.println("INVALID INPUT");
+                continue;
+            } else if (row < 0 || row > machine.getRowLength() - 1 || spot < 0 || spot > machine.getRowLength() - 1) { 
+                System.out.println("INVALID INDEX");
+                continue;
+            } else if (machine.getItem(row, spot).getQuantity() == 0) {
+                System.out.println("EMPTY SLOT");
+                continue;
+            }
 
             machine.dispense(row, spot);
             System.out.println("\n" + machine);
