1eb57a8cf7ceb6b513a6313ec3a3e872a7a33956
uchario <uchario@yahoo.com>
2022-08-23 11:30:36 +0100

    throw Exceptions for invalid entries in Item(), setName(), setPrice(), setQuantity() in Item.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/models/Item.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/models/Item.java
@@ -5,6 +5,17 @@ public class Item {
     private int quantity;
 
     public Item(String name, double price, int quantity) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("'name' cannot be null/blank.");
+        }
+
+        if (price < 0) {
+            throw new IllegalArgumentException("'price' cannot be less than zero.");
+        }
+
+        if (quantity < 0) {
+            throw new IllegalArgumentException("quantity cannot be less than zero.");
+        }
         this.name = name;
         this.price = price;
         this.quantity = quantity;   
@@ -24,12 +35,21 @@ public class Item {
         return quantity;
     }
     public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("'name' cannot be null/blank");
+        }
         this.name = name;
     }
     public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("'price' cannot be less than zero");
+        }
         this.price = price;
     }
     public void setQuantity(int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("'quantity' cannot be less than zero");
+        }
         this.quantity = quantity;
     }
 
