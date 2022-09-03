d9174d4e6215dd2716114f0c7b0fb3f0e016661b
uchario <uchario@yahoo.com>
2022-09-03 11:56:59 +0100

    Add Store class with methods and validation

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/movie-store/models/Store.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/movie-store/models/Store.java
@@ -1,5 +1,70 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Store {
-    
+    private  ArrayList<Movie> movies;
+
+    public Store() {
+        this.movies = new ArrayList<Movie>();
+    }
+
+    public Movie getMovie(int index) {
+        return new Movie(this.movies.get(index));
+    }
+
+    public void setMovie(int index, Movie movie) {
+        this.movies.set(index, new Movie(movie)); 
+    }
+
+    public void addMovie(Movie movie) {
+        this.movies.add(new Movie(movie));
+    }
+
+    public void action(String name, String action) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name must not be null/blank");
+        }
+
+        if (this.movies.isEmpty()) {
+            throw new IllegalStateException("Movie list is empty!");
+        }
+
+        if (!(action.equalsIgnoreCase("sell") || action.equalsIgnoreCase("rent") || action.equalsIgnoreCase("rent"))) {
+            System.out.println("**" + action + "**");
+            throw new IllegalArgumentException("Invalid action!");
+        }
+
+        for (int i = 0; i < this.movies.size(); i++) {
+            if (this.movies.get(i).getName().equalsIgnoreCase(name)) {
+                switch(action) {
+                    case "sell":
+                        if (this.movies.get(i).isAvailable() == false) {
+                            throw new IllegalStateException("Movie is not available!");
+                        }
+                        this.movies.remove(i);
+                        return;
+                    case "rent":
+                        if (this.movies.get(i).isAvailable() == false) {
+                            throw new IllegalStateException("Movie is not available!");
+                        }
+                        this.movies.get(i).setAvailable(false);
+                        return;
+                    case "return":
+                        this.movies.get(i).setAvailable(true);
+                        return;
+                }
+            }
+        }
+        throw new IllegalArgumentException("Movie not in store!");
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < movies.size(); i++) {
+            temp += movies.get(i).toString();
+            temp += "\n\n";
+        }
+        return temp;
+    }
 }
