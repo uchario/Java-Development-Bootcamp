0da14282626ec717810db6c256674a64d440f4b3
uchario <uchario@yahoo.com>
2022-09-03 11:58:06 +0100

    Add loadMovies() and manageMovies() and implemented in main()

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/movie-store/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/movie-store/Main.java
@@ -1,7 +1,54 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import models.Movie;
+import models.Store;
 public class Main {
+    static Store store = new Store();
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("\n********************JAVA VIDEO STORE********************\n");
+       
+        try {
+            loadMovies("movies.txt");
+            System.out.println(store);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
 
+        manageMovies();
+    }
+
+    public static void loadMovies(String fileName) throws FileNotFoundException{
+        FileInputStream fis = new FileInputStream(fileName);
+        Scanner scanFile = new Scanner(fis);
+        while (scanFile.hasNextLine()) {
+            String[] lineArray = scanFile.nextLine().split("--");
+            store.addMovie(new Movie(lineArray[0], lineArray[1], Double.parseDouble(lineArray[2])));
+        }
+        scanFile.close();
+    }
+
+    public static void manageMovies() {
+        while (true) {
+            System.out.println("\nWould you like to \n\ta). purchase\n\tb). rent \n\tc). return");
+            String option = scan.nextLine();
+            System.out.print("Enter the name of the movie: ");
+            String movieChoice = scan.nextLine();
+
+            switch (option) {
+                case "a":
+                    store.action(movieChoice, "sell");
+                    break;
+                case "b":
+                    store.action(movieChoice, "rent");
+                    break;
+                case "c":
+                    store.action(movieChoice, "return");
+                    break;
+            }
+        }
     }
 
     /**
