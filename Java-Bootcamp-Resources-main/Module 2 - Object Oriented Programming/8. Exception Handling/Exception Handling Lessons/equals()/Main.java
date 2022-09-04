ac53fabbf438b8545b6f817aeb349ddc4efe6d74
uchario <uchario@yahoo.com>
2022-09-04 20:20:38 +0100

    Implement equals() method

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/equals()/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/equals()/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Car car = new Car("Ford", "blue");
+        Car car1 = new Car(car);
+        Car car2 = new Car("Honda", "matted");
+        //car1.setMake("Benz");
+        Scanner scan = new Scanner(System.in);
+        System.out.println(car.equals(car2));
     }
 
 }
