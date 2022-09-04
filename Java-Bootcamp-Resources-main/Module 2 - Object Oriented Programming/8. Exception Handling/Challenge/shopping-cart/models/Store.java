b7459df53a896a242de252fb8fc6afc2119fb15b
uchario <uchario@yahoo.com>
2022-09-04 21:21:02 +0100

    Add Item[][], add getters and setters, add toString()

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Store.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Store.java
@@ -1,6 +1,28 @@
 package models;
 
 public class Store {
+    private Item[][] items;
 
+    public Store() {
+        this.items = new Item[7][3];
+    }
 
+    public Item getItem(int row, int column) {
+        return new Item(this.items[row][column]);
+    }
+
+    public void setItem(int row, int column, Item item) {
+        this.items[row][column] = new Item(item);
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < this.items.length; i++) {
+            for (int j = 0; j < this.items[i].length; j++) {
+                temp += this.items[i][j].toString();
+            }
+            temp += "\n\n";
+        }
+        return temp;
+    }
 }
