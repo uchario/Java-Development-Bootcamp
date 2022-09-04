1eff3fa560ca660aaa4fe41166deff509b3ef3d5
uchario <uchario@yahoo.com>
2022-09-04 18:29:09 +0100

    Instantiate Item class, add constructor, getters & setters for Cart.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Cart.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Cart.java
@@ -1,8 +1,62 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Cart {
+    private ArrayList<Item> items;
 
+    public Cart() {
+        this.items = new ArrayList<Item>();
+    }
 
+    public Item getitem(int index) {
+        return new Item(this.items.get(index));
+    }
+
+    public void setItem(int index, Item item) {
+        this.items.set(index, new Item(item));
+    }
+
+    public boolean add(Item item) {
+        if (!this.items.contains(item)) {
+            this.items.add(new Item(item));
+            return true;
+        }
+        return false;
+    }
+
+    public void remove(String name) {
+        for (int i = 0; i < this.items.size(); i++) {
+            if (this.items.get(i).getName().equals(name)) {
+                this.items.remove(this.items.get(i));
+            }
+        }
+    }
+
+    public String checkout() {
+        double subtotal = 0;
+        double tax;
+        double total;
+        for (int i = 0; i < this.items.size(); i++) {
+            subtotal += this.items.get(i).getPrice();
+        }
+        tax = 0.13 * subtotal;
+        total = subtotal + tax;
+        
+        return "\tRECEIPT\n\n" + 
+                "\tSubtotal: $" + subtotal + "\n" +
+                "\tTax: $" + tax + "\n" +
+                "\tTotal: $" + total + "\n";
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < this.items.size(); i++) {
+            temp += this.items.get(i).toString();
+            temp += "\n";
+        }
+        return temp;
+    }
    /**
     * Name: add
     * @param item
