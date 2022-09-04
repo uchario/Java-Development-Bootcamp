fa8617a7720942673902f24d4504cccd97e94fc5
uchario <uchario@yahoo.com>
2022-09-04 17:55:35 +0100

    Initiate a constructor, getters and setters for Item.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Item.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Item.java
@@ -1,6 +1,37 @@
 package models;
 
 public class Item {
+private String name;
+private double price;
 
+public Item(String name, double price) {
+    this.name = name;
+    this.price = price;
+}
+
+public Item(Item source) {
+    this.name = source.name;
+    this.price = source.price;
+}
+
+public String getName() {
+    return this.name;
+}
+
+public double getPrice() {
+    return this.price;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public void setPrice(double price) {
+    this.price = price;
+}
+
+public String toString() {
+    return this.name + ": $" + this.price + " "; 
+}
   
 }
