bfc2b7b7027dd2410d522e7bd4fb3bf7df5474cb
uchario <uchario@yahoo.com>
2022-09-04 21:22:29 +0100

    Add Item[][] inventory, initialize Store store, add inventory to Store using setItem, print out individual components of the array

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
@@ -3,13 +3,27 @@ import models.Item;
 import models.Store;
 
 public class Main {
-    static Cart cart = new Cart();
+    static Store store = new Store();
     public static void main(String[] args) {
-        cart.add(new Item("Celery", 0.99));
-        cart.add(new Item("Celery", 0.99));
-        cart.add(new Item("Spinach", 0.99));
-        cart.add(new Item("Coriander", 1.29));
-        System.out.println(cart);
+        Item[][] inventory = new Item[][] { 
+            { new Item("Pepsi", 1.99), new Item("Crush", 1.99), new Item("Cola", 1.99) },
+            { new Item("Honey Oats", 3.99), new Item("Fruit Loops", 1.99), new Item("Cheerios", 2.99) },
+            { new Item("Milk", 4.99), new Item("Eggs", 0.99), new Item("Cheese", 1.89) },
+            { new Item("Pepperoni", 2.99), new Item("Salami", 4.49), new Item("Mortadella", 4.99) },
+            { new Item("Celery", 0.99), new Item("Spinach", 0.99), new Item("Coriander", 1.29) },
+            { new Item("Shirt", 12.99), new Item("Pants", 24.99), new Item("Sweater", 18.99) }, 
+            { new Item("Phone", 549.99), new Item("Printer", 349.99), new Item("Television", 1099) } 
+        };
+
+        for (int i = 0; i < inventory.length; i++) {
+            for (int j = 0; j < inventory[i].length; j++) {
+                store.setItem(i, j, inventory[i][j]);
+            }
+        }
+
+        System.out.println("\n\t******************************JAVA GROCERS******************************\n");
+        
+        System.out.println(store + "\n");
     }
 
     /**
