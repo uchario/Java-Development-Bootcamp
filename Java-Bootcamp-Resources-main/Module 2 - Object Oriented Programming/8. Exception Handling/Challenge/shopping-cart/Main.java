fa97b8b4866be8efc0039fe2258cfe4eda9cfdc1
uchario <uchario@yahoo.com>
2022-09-10 12:38:17 +0100

    Add main() and loadItems() function to Main.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
@@ -1,29 +1,38 @@
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
 import models.Cart;
 import models.Item;
 import models.Store;
 
 public class Main {
     static Store store = new Store();
+    static Cart cart = new Cart();
+    
     public static void main(String[] args) {
-        Item[][] inventory = new Item[][] { 
-            { new Item("Pepsi", 1.99), new Item("Crush", 1.99), new Item("Cola", 1.99) },
-            { new Item("Honey Oats", 3.99), new Item("Fruit Loops", 1.99), new Item("Cheerios", 2.99) },
-            { new Item("Milk", 4.99), new Item("Eggs", 0.99), new Item("Cheese", 1.89) },
-            { new Item("Pepperoni", 2.99), new Item("Salami", 4.49), new Item("Mortadella", 4.99) },
-            { new Item("Celery", 0.99), new Item("Spinach", 0.99), new Item("Coriander", 1.29) },
-            { new Item("Shirt", 12.99), new Item("Pants", 24.99), new Item("Sweater", 18.99) }, 
-            { new Item("Phone", 549.99), new Item("Printer", 349.99), new Item("Television", 1099) } 
-        };
+        try {
+            loadItems("products.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("\n\t*******************JAVA GROCERS*******************");
+        System.out.println(store + "\n");
+    }
 
-        for (int i = 0; i < inventory.length; i++) {
-            for (int j = 0; j < inventory[i].length; j++) {
-                store.setItem(i, j, inventory[i][j]);
+    public static void loadItems(String fileName) throws FileNotFoundException{
+        FileInputStream fis = new FileInputStream(fileName);
+        Scanner scanFile = new Scanner(fis);
+        for (int i = 0; scanFile.hasNextLine(); i++) {
+            String line = scanFile.nextLine();
+            String[] items = line.split(";");
+            for (int j = 0; j < items.length; j++) {
+                String[] fields = items[j].split("=");
+                store.setItem(i, j, new Item(fields[0], Double.parseDouble(fields[1])));
             }
         }
-
-        System.out.println("\n\t******************************JAVA GROCERS******************************\n");
-        
-        System.out.println(store + "\n");
+        scanFile.close();
     }
 
     /**
