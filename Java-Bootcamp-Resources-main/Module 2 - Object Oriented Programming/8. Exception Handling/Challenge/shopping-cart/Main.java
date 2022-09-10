c48e713ce719a07dfb2e1d0778d9f986810751de
uchario <uchario@yahoo.com>
2022-09-10 14:46:01 +0100

    Create manageItems() method and add manageItems() to main()

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
@@ -14,11 +14,10 @@ public class Main {
     public static void main(String[] args) {
         try {
             loadItems("products.txt");
+            manageItems();
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("\n\t*******************JAVA GROCERS*******************");
-        System.out.println(store + "\n");
     }
 
     public static void loadItems(String fileName) throws FileNotFoundException{
@@ -33,6 +32,46 @@ public class Main {
             }
         }
         scanFile.close();
+    }
+
+    public static void manageItems() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n\t******************************JAVA GROCERS******************************\n");
+            System.out.println(store);
+            System.out.println("Options: \n\ta). Add to cart\n\tb). Remove from cart\n\tc). Checkout");
+            String option = scan.nextLine();
+            switch(option) {
+                case "a":
+                    System.out.print("\nChoose an aisle number between: 1 - 7: ");
+                    int row = scan.nextInt() - 1;
+                    scan.nextLine();
+                    System.out.print("Choose an item number between: 1 - 3: ");
+                    int column = scan.nextInt() - 1;
+                    scan.nextLine();
+                    Item item = new Item(store.getItem(row, column));
+                    if (!(cart.add(item))) {
+                        System.out.println(item.getName() + " is already in your shopping cart");
+                    } else {
+                        System.out.println(item.getName() + " was added to your shopping cart.");
+                    }
+                    break;
+                case "b":
+                    System.out.print("Enter the item you'd like to remove: ");
+                    String removedItem = scan.nextLine();
+                    cart.remove(removedItem);
+                    break;
+                case "c":
+                    System.out.println(cart.checkout());
+                    scan.close();
+                    return;
+                default:
+                    continue;
+            }
+            System.out.println("\n\nSHOPPING CART\n\n" + cart);
+            System.out.println("Enter anything to continue");
+            scan.nextLine();
+        }
     }
 
     /**
