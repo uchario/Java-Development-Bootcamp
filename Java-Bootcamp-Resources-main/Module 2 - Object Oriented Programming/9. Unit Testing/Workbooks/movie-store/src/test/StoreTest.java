bf6d0792e99471213adea010bf274efc02bd0d6e
uchario <uchario@yahoo.com>
2022-09-13 14:04:17 +0100

    Add setup(), movieAdded() and sellMovie() to StoreTest.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/test/StoreTest.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/test/StoreTest.java
@@ -1,9 +1,34 @@
 package src.test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import src.main.models.Store;
+import src.main.models.Movie;
 
 public class StoreTest {
 
     Store store;
+
+    @Before
+    public void setup() {
+        store = new Store();
+        store.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
+        store.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
+    }
+
+    @Test
+    public void movieAdded() {
+        assertTrue(store.contains(new Movie("The Godfather", "Blue-Ray", 9.1)));
+    }
+
+    @Test
+    public void sellMovie() {
+        store.sellMovie("The Shawshank Redemption");
+        assertFalse(store.contains(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2)));
+    }
 
 }
