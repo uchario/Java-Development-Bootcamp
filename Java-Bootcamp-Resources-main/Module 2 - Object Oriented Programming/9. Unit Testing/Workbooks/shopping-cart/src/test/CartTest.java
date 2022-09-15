43aef18ee5b09464b9365d2dab88ec5dfb372e08
uchario <uchario@yahoo.com>
2022-09-15 14:36:26 +0100

    Add setup(), itemAddedTest() and skipsDuplicate() to CartTest.java

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/shopping-cart/src/test/CartTest.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/shopping-cart/src/test/CartTest.java
@@ -1,9 +1,32 @@
 package src.test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
 import src.main.models.Cart;
+import src.main.models.Item;
 
 public class CartTest {
 
     Cart cart;
+
+    @Before
+    public void setup() {
+        cart = new Cart();
+        cart.add(new Item("Toblerone", 5.99));
+        cart.add(new Item("Pepsi", 1.99));
+    }
     
+    @Test
+    public void itemAddedTest() {
+        assertTrue(cart.contains(new Item("Toblerone", 5.99)));
+    }
+
+    @Test
+    public void skipsDuplicate() {
+        assertFalse(cart.add(new Item("Toblerone", 5.99)));
+    }
 }
