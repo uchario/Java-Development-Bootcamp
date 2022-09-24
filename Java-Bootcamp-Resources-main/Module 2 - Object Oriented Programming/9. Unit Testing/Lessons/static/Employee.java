80aa0f255bc70e9b231074885060d99a2207e86f
uchario <uchario@yahoo.com>
2022-09-24 22:33:05 +0100

    Change hiredCount to static, add COMPANY_NAME constant, add getCompanyName()

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/9. Unit Testing/Lessons/static/Employee.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/9. Unit Testing/Lessons/static/Employee.java
@@ -1,11 +1,13 @@
 public class Employee {
     private String name;
     private String position;
-    static int hiredCount; 
+    private static int hiredCount;
+    private static final String COMPANY_NAME = "JAVASTARS";
 
     public Employee(String name, String position) {
         this.name = name;
         this.position = position;
+        hiredCount++;
     }
   
     public Employee(Employee source) {
@@ -20,16 +22,23 @@ public class Employee {
     public String getPosition() {
         return position;
     }
+
+    public static String getCompanyName() {
+        return COMPANY_NAME;
+    }
   
     public void greet(String company) {
-        if (company.equals("JAVASTARS")) {
+        if (company.equals(COMPANY_NAME)) {
             System.out.println("Welcome!");
         }
     }
 
+    public static int getHiredCount() {
+        return hiredCount;
+    }
 
     public String toString() {
 
-        return "\tName: " + name + "\tPosition: " + this.position + "\tCompany: " + "JAVASTARS" + "\n";
+        return "\tName: " + name + "\tPosition: " + this.position + "\tCompany: " + COMPANY_NAME + "\n";
     }
 }
