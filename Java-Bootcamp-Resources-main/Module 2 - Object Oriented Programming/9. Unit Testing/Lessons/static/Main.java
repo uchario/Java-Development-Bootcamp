0eab9d249481b117fcc92cef6f700c3928fb03f8
uchario <uchario@yahoo.com>
2022-09-24 22:33:59 +0100

    Create 3 instances of Employee and print getHiredCount

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/9. Unit Testing/Lessons/static/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/9. Unit Testing/Lessons/static/Main.java
@@ -1,8 +1,10 @@
 public class Main {
   
     public static void main(String[] args) {
-
-      
+        Employee jack = new Employee("Jack", "Programmer");
+        Employee gio = new Employee("Gio", "Analyst");
+        Employee thomas = new Employee("Thomas", "Specialist");
+        System.out.println(Employee.getHiredCount());
     }
   
 }
