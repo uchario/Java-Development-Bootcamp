884457dd2e499cec5e18d25c8eb8b1cca4a4e9ae
uchario <uchario@yahoo.com>
2022-10-11 16:07:53 +0100

    Add Pants class which extends Product and implements Discountable

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/products/models/Pants.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/products/models/Pants.java
@@ -1,1 +1,69 @@
 package models;
+
+import java.util.Objects;
+
+public class Pants extends Product implements Discountable {
+    private int waist;
+
+    public Pants(int waist, double price, String color, String brand) {
+        super(price, color, brand);
+        this.waist = waist;
+    }
+
+    public Pants(Pants source) {
+        super(source);
+        this.waist = source.waist;
+    }
+
+    public int getWaist() {
+        return this.waist;
+    }
+
+    public void setWaist(int waist) {
+        this.waist = waist;
+    }
+
+    @Override
+    public void discount() {
+        super.setPrice(super.getPrice() / 2);
+    }
+
+    @Override
+    public void fold() {
+        System.out.println("Hold pants upright");
+        System.out.println("Fold one leg over the other");
+        System.out.println("Fold pants from bottom in thirds");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Pants)) {
+            return false;
+        }
+        Pants pants = (Pants) o;
+        return this.waist == pants.waist
+                && super.getBrand().equals(pants.getBrand())
+                && super.getColor().equals(pants.getColor())
+                && super.getPrice() == pants.getPrice();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.waist, super.getBrand(), super.getColor(), super.getPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " waist='" + this.getWaist() + "'" +
+            " brand='" + super.getBrand() + "'" +
+            " color='" + super.getColor() + "'" +
+            " price='" + super.getPrice() + "'" +
+            "}";
+    }
+
+
+
+}
