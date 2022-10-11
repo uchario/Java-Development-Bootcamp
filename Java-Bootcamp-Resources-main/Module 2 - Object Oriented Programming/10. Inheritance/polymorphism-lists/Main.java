576928edaf0c514a94db74cf349125a65dc18f7c
uchario <uchario@yahoo.com>
2022-10-11 17:56:18 +0100

    Use List class in place of ArrayList and LinkedList

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/polymorphism-lists/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/polymorphism-lists/Main.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
         list.add(1);
-        list.add(2);        
+        list.add(2);
+        List<Integer> list2 = new LinkedList<Integer>();
+        list2.add(1);
+        list2.add(2);
+        list2.size();
+        list2.isEmpty();
     }
 }
