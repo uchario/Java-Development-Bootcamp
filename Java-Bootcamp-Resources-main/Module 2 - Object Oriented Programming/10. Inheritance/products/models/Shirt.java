1a0660ccb270ea0f69537ea747fc70cc3224804c
uchario <uchario@yahoo.com>
2022-10-11 16:09:36 +0100

    Add Shirt class which extends Product

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/products/models/Shirt.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/products/models/Shirt.java
@@ -1,1 +1,68 @@
 package models;
+
+import java.util.Objects;
+
+public class Shirt extends Product{
+    public enum Size {
+        SMALL, MEDIUM, LARGE
+    }
+
+    private Size size;
+
+    public Shirt(Size size, double price, String color, String brand) {
+        super(price, color, brand);
+        this.size = size;
+    }
+
+    public Shirt(Shirt source) {
+        super(source);
+        this.size = source.size;
+    }
+
+    public Size getSize() {
+        return this.size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public void fold() {
+        System.out.println("Lay shirt on flat surface");
+        System.out.println("Fold shirt sideways");
+        System.out.println("Bring sleeves in");
+        System.out.println("Fold from bottom up");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Shirt)) {
+            return false;
+        }
+        Shirt shirt = (Shirt) o;
+        return this.size.equals(shirt.size)
+                && super.getPrice() == shirt.getPrice()
+                && super.getBrand().equals(shirt.getBrand())
+                && super.getColor().equals(shirt.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, super.getBrand(), super.getColor(), super.getPrice());
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " size='" + getSize() + "'" +
+            " price='" + super.getPrice() + "'" +
+            " brand='" + super.getBrand() + "'" +
+            " color='" + super.getColor() + "'" +
+            "}";
+    }
+
+}
